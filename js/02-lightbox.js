27b040dac7d541bd808f38f42a22a79a3c5bdcb6
Kamil Przebieracz <dmjs23@gmail.com>
2022-03-16 00:56:00 +0100

    zad2 -done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector("ul.gallery");
+
+galleryItems.forEach((el) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = el.preview;
+  galleryImage.alt = el.description;
+
+  galleryBox.append(galleryLink);
+  galleryLink.append(galleryImage);
+});
+
+galleryBox.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  console.log(ev.target.alt);
+});
+
+galleryBox.onclick = (ev) => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    spinner: false,
+    captionsData: "alt",
+    captionPosition: "outside",
+    captionDelay: 250,
+  });
+};
